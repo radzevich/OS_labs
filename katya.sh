9754a0d2373326043934a6b43b9ccc3c244578a2
kkk <kk@gamil.com>
2017-02-25 23:11:45 +0300

    katya

--- katya.sh
+++ katya.sh
@@ -6,14 +6,12 @@ if [ -s $ERR_LOG ]
 then
     exit 0 
 fi
-echo "$(date)" > $3
+echo "$(date)" >> $3
 count=`find $1 -maxdepth $2 -type d | wc -l`
 ((count=count-1))
 for d in $( find $1 -maxdepth $2 -type d); do
       echo "$d :" >> $3
-   #   echo "$( wc -l )"
       echo "$(find $d -maxdepth 1 -type f | wc -l)" >> $3
-     # echo "$($d | wc -l)"
 done
   echo "count of wathed dirs is: $count" >> $3
 exec 2>&6 6>&-
