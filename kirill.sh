01b441a00a3d4cfbe69ca10a68412ed25dc162a6
kkk <kk@gamil.com>
2017-03-01 01:51:53 +0300

    Fixed Kirill's script

--- kirill.sh
+++ kirill.sh
@@ -6,8 +6,11 @@ if [ -s $ERR_LOG ]
 then
     exit 0 
 fi
-find $1 -size -$2c -a -size +$3c -exec du -c {} + | ( tail -n 1; ls | wc -l; du -a -h --total --max-depth=40 $1 ) 
-
+min_size=$2
+echo "$(readlink -f $1)"
+find $1 -type f  -size -$2k -a -size +$3k -exec du -ch {} + | ( grep total )
+find $1 -type f  -size -$2k -a -size +$3k -exec du -ch {} + | ( wc -l )
+#find $1 -type f -size +$2k -a -size -$3k + | wc -l
 exec 2>&6 6>&-
 sed "s/.[a-zA-Z ]*:/`basename $0`:/" < $ERR_LOG 1>&2
 
