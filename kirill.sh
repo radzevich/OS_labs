31d61bc685903a83c50282ccbeecdaaab45aa31e
kkk <kk@gamil.com>
2017-03-01 09:56:04 +0300

    Fix

--- kirill.sh
+++ kirill.sh
@@ -8,8 +8,8 @@ then
 fi
 min_size=$2
 echo "$(readlink -f $1)"
-find $1 -type f  -size -$2k -a -size +$3k -exec du -ch {} + | ( grep total )
-find $1 -type f  -size -$2k -a -size +$3k -exec du -ch {} + | ( wc -l )
+find $1 -type f  -size -$2c -a -size +$3c -exec du -ch {} + | ( grep total )
+find $1 -type f  -size -$2c -a -size +$3c -exec du -ch {} + | ( wc -l )
 #find $1 -type f -size +$2k -a -size -$3k + | wc -l
 exec 2>&6 6>&-
 sed "s/.[a-zA-Z ]*:/`basename $0`:/" < $ERR_LOG 1>&2
