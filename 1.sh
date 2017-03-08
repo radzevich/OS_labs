b09ecf0de94a9cc1795d88e92005d0b582c08919
Алексей <virtuain98@gmail.com>
2017-03-08 15:15:24 +0300

    Trash code removed

--- 1.sh
+++ 1.sh
@@ -13,10 +13,6 @@ for file in $(find "$4" -size +$1c -size -$2c -type f)
 do
 	((counter++))
 	printf "Dir: %s \nName: %s \nSize: %s\n\n" $(dirname "$file") $(basename "$file") $(stat -c %s "$file") >> "$3"
-	#echo "Dir: $(dirname "$file")" >> "$3" # Name: $(basename "$file")  Size: $(stat -c %s "$file")" >> "$3"
-	#echo "Name: $(basename "$file")" >> "$3"
-	#echo "Size: $(stat -c %s "$file")" >> "$3"
-	#echo >> "$3"
 done
 
 echo $counter
