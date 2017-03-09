10ddc17f456438a4693bbfe951063176d0f716be
Алексей <virtuain98@gmail.com>
2017-03-09 14:57:55 +0300

    Error log

--- 1.sh
+++ 1.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
 counter=0
+ERROR_LOG="error.log"
+IFS=$'\n'
+exec 2<> $ERROR_LOG
 
 if [ -e "$3" ] 
 then
@@ -8,13 +11,17 @@ then
 fi
 
 counter=0
-
 for file in $(find "$4" -size +$1c -size -$2c -type f)
 do
 	((counter++))
-	printf "Dir: %s \nName: %s \nSize: %s\n\n" $(dirname "$file") $(basename "$file") $(stat -c %s "$file") >> "$3"
+	printf "%s  %s  %s\n" $(dirname "$file") $(basename "$file") $(stat -c %s "$file") >> "$3"
 done
-
 echo $counter
+
+cat error.log | while read line
+do
+	echo "$1.sh: $line"
+done
+exec $ERROR_LOG>&-
 
 exit 0
