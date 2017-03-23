e5da0b894a21abf904da575d30c419ff9ee4d8bf
Алексей <virtuain98@gmail.com>
2017-03-23 11:42:51 +0300

    Counting of total directory size

--- 2nd_lab/main.c
+++ 2nd_lab/main.c
@@ -2,45 +2,82 @@
 #include <stdlib.h>
 #include <string.h>
 
-#define BUF_SIZE 256
+#define BUF_SIZE 400
+#define TRUE 1
 
+void strRewrite(char *buf, char *str);
 void generateError(char *err_message);
+int countDirectrySize(FILE *path_list);
 
 
 int main(int argc, char *argv[])
 {
-	char buf[BUF_SIZE];
 	FILE *ptr;
 
-	//Checking command line arguments num
+	//Checking command line arguments num.
 	if (argc < 2)
 		generateError("too few arguments");
 
-	//printf("%s\n", argv[1]);
-
-	char cmd[100] = " find ";
+	char cmd[BUF_SIZE] = " find ";
 	strcat(cmd, strcat(argv[1], " -type f")); 
 	ptr = popen(cmd, "r");
+
+	printf("1\n");
 
 	if (NULL == ptr)
 		generateError("file not exist");
 
-	while (!feof(ptr))
-	{
-		fgets(buf, sizeof(buf), ptr);
-		printf("%s", buf);
-	}
-
+	printf("The total size is: %d bytes\n", countDirectrySize(ptr));
 	pclose(ptr);
-
 	
 	return 0;
 }
 
 
-//Generating error with error message
+//Generating error with error message.
 void generateError(char *err_message)
 {
-	printf("ERROR: %s\n", err_message);
+	fprintf(stderr, "ERROR: %s\n", err_message);
 	exit(1);
+}
+
+
+//Countig total size of directry and it's subdirectries.
+int countDirectrySize(FILE *path_list)
+{
+	int size;
+	char buf[20];
+	FILE *ptr;
+	char cmd[BUF_SIZE];
+	char path[BUF_SIZE];
+
+	printf("2\n");
+
+	while (TRUE)
+	{
+		if (fgets(path, BUF_SIZE, path_list) != NULL)
+		{
+			strRewrite(cmd, path);
+			ptr = popen(cmd, "r");
+
+			if (NULL == (ptr = popen(cmd, "r")))
+				generateError("popen failed");	
+
+			fgets(buf, sizeof(buf), ptr);	
+			size += atoi(buf);
+			pclose(ptr);
+		}
+		else
+			break;
+	}
+	
+	return size;
+}
+
+
+void strRewrite(char *buf, char *str)
+{
+	memset(buf, 0 , sizeof(buf));
+	strcpy(buf, " stat -c%s ");
+	strcat(buf, str);
 }
