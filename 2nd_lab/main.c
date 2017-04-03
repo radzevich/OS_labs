ce8cb06241a3c4bcac1a3f2f11cd11d90c9f3b35
Alex Radevich <virtuain98@gmail.com>
2017-04-03 10:56:14 +0300

    Add files via upload

--- 2nd_lab/main.c
+++ 2nd_lab/main.c
@@ -1,83 +1,122 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <errno.h>
 #include <string.h>
+#include <limits.h>
 
-#define BUF_SIZE 400
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <dirent.h>
+#include <unistd.h>
+
+#define BUF_SIZE 256
 #define TRUE 1
+#define HARD_DRIVE_INFO_FILE_L "/sys/class/block/sda/size"
 
 void strRewrite(char *buf, char *str);
 void generateError(char *err_message);
-int countDirectrySize(FILE *path_list);
+int countDirectrySize(char *dir_path, unsigned long *dir_size, unsigned long *file_size);
+unsigned long getHardDriveSize();
 
 
 int main(int argc, char *argv[])
 {
-	FILE *ptr;
+    //Redirection error stream to error log file.
+    FILE *error_log = freopen("error.log", "w", stderr);
 
-	//Checking command line arguments num.
-	if (argc < 2)
-		generateError("too few arguments");
+	double part_size;
+    unsigned long file_size = 0, dir_size = 0, hard_drive_size;
 
-	char cmd[BUF_SIZE] = " find ";
-	strcat(cmd, strcat(argv[1], " -type f")); 
-	ptr = popen(cmd, "r");
+    //Checking command line arguments num.
+    if (argc < 2)
+        generateError("too few arguments");
 
-	printf("1\n");
+    if (countDirectrySize(/*"/home/virtuain/Desktop"*/argv[1], &dir_size, &file_size) == 0)
+    {
+        printf("The total size of regular files is: %ld bytes\n", file_size);
+        printf("The total size of occupied memory space: %ld bytes\n", file_size + dir_size);
+        hard_drive_size = getHardDriveSize();
+        part_size = (double)(dir_size + file_size) / hard_drive_size * 100;
+        printf("The part of memory space is: %2f%%\n", part_size);
+    }
 
-	if (NULL == ptr)
-		generateError("file not exist");
-
-	printf("The total size is: %d bytes\n", countDirectrySize(ptr));
-	pclose(ptr);
+    fclose(error_log);
 	
 	return 0;
+}
+
+int countDirectrySize(char *dir_path, unsigned long int *dir_size, unsigned long int *file_size)
+{
+	DIR *dir = NULL;
+	struct dirent entry;
+	struct dirent *entry_ptr;
+	int retval = 0;
+	char pathName[BUF_SIZE + 1];
+
+    dir = opendir(dir_path);
+	if (NULL == dir)
+        generateError("opening directory");
+
+    retval = readdir_r(dir, &entry, &entry_ptr);
+	while (entry_ptr != NULL)
+	{
+		struct stat entry_info;
+
+		// Short-circuit the . and .. entries. 
+        if ((strncmp(entry.d_name, "..", BUF_SIZE) == 0))
+		{
+            retval = readdir_r(dir, &entry, &entry_ptr);
+            continue;
+        }
+
+        (void)strncpy(pathName, dir_path, BUF_SIZE);
+        (void)strncat(pathName, "/", BUF_SIZE);
+        (void)strncat(pathName, entry.d_name, BUF_SIZE);
+
+        if ((lstat(pathName, &entry_info) == 0) & !S_ISLNK(entry_info.st_mode))
+        {
+            if (S_ISDIR(entry_info.st_mode))
+            {
+                *dir_size += entry_info.st_size;
+
+                if (!(strncmp(entry.d_name, ".", BUF_SIZE) == 0))
+                {
+                    countDirectrySize(pathName, dir_size, file_size);
+                }
+            }
+            else if (S_ISREG(entry_info.st_mode))
+            {
+                *file_size += entry_info.st_size;
+            }
+        }
+        retval = readdir_r(dir, &entry, &entry_ptr);
+    }
+    (void)closedir(dir);
+    return 0;
+}
+
+
+//Getting hard drive's size
+unsigned long int getHardDriveSize()
+{
+    FILE *ptr = NULL;
+    unsigned long int size = 0;
+
+    ptr = fopen(HARD_DRIVE_INFO_FILE_L, "r");
+
+    if (NULL == ptr)
+        generateError("opening of hardware info file");
+
+    if (fscanf(ptr, "%ld", &size) <= 0)
+        generateError("reading info file");
+
+    return size << 9;
 }
 
 
 //Generating error with error message.
 void generateError(char *err_message)
 {
-	fprintf(stderr, "ERROR: %s\n", err_message);
+	fprintf(stderr, "2.c ERROR: %s: %s\n", err_message, strerror(errno));
 	exit(1);
-}
-
-
-//Countig total size of directry and it's subdirectries.
-int countDirectrySize(FILE *path_list)
-{
-	int size;
-	char buf[20];
-	FILE *ptr;
-	char cmd[BUF_SIZE];
-	char path[BUF_SIZE];
-
-	printf("2\n");
-
-	while (TRUE)
-	{
-		if (fgets(path, BUF_SIZE, path_list) != NULL)
-		{
-			strRewrite(cmd, path);
-			ptr = popen(cmd, "r");
-
-			if (NULL == (ptr = popen(cmd, "r")))
-				generateError("popen failed");	
-
-			fgets(buf, sizeof(buf), ptr);	
-			size += atoi(buf);
-			pclose(ptr);
-		}
-		else
-			break;
-	}
-	
-	return size;
-}
-
-
-void strRewrite(char *buf, char *str)
-{
-	memset(buf, 0 , sizeof(buf));
-	strcpy(buf, " stat -c%s ");
-	strcat(buf, str);
 }
